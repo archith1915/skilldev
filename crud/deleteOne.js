c67d4be59f1959f06d8cba392a4a9456ad9b6ab7
J A Archith <jaarchith@gmail.com>
2024-11-12 14:43:16 +0530

    Day two pre lunch session

---
+++ crud/deleteOne.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const mongo_url =
+  "mongodb+srv://archith1915:archith1915@cluster0.yh2o4.mongodb.net/";
+
+const connectToMongo = async () => {
+  mongoose.Promise = global.Promise;
+
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB", err);
+    process.exit(1);
+  }
+};
+
+const collection_name = "trainer";
+const collection_fields = {
+  name: String,
+  location: String,
+  technology: String,
+  phone_number: String,
+};
+const collection_config = {
+  timestamps: false,
+};
+
+const schema = mongoose.Schema(collection_fields, collection_config);
+const TrainerModel = mongoose.model(collection_name, schema);
+
+const deleteOne = async () => {
+  await connectToMongo();
+
+  try {
+    const trainers = await TrainerModel.deleteOne({ name: "Archith" });
+    console.log("Trainers : ", trainers);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoose.disconnect();
+    console.log("MongoDB connection closed");
+  }
+};
+
+deleteOne();
--- test.js
+++
@@ -1,7 +0,0 @@
-let x = 10;
-function example() {
-  let x = 20;
-  console.log(x);
-}
-example();
-console.log(x);
